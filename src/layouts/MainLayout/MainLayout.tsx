36a9468b9fca1394a772d104d504b5d83fe05a3f
markbenson88 <mark.benson@jimpsons.com>
2024-11-24 00:44:44 -0500

    Footer layout

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
-import { Header } from "@components/composite/Header";
+import { Header } from "@/components/composite/Header";
+import { Footer } from "@/components/composite/Footer";
+import styles from "./MainLayout.module.css";
 
 const MainLayout: FC = () => {
   return (
-    <div>
+    <div className={styles.appContainer}>
       <Header />
+      <main className={styles.mainContent}>{/* Main Content */}</main>
+      <Footer />
     </div>
   );
 };
