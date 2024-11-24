6281b48c569d5e640fe800e714f761f4d9e4589d
markbenson05 <mark.benson@jimpsons.com>
2024-11-24 01:45:13 -0500

    change px to rem in footer image logo

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,13 +1,11 @@
 import { FC } from "react";
 import { Header } from "@/components/composite/Header";
 import { Footer } from "@/components/composite/Footer";
-import styles from "./MainLayout.module.css";
 
 const MainLayout: FC = () => {
   return (
-    <div className={styles.appContainer}>
+    <div>
       <Header />
-      <main className={styles.mainContent}>{/* Main Content */}</main>
       <Footer />
     </div>
   );
